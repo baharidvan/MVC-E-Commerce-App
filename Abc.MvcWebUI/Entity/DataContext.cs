c4d4d2b7ca1b0d2458daca4c8d3f52c631a793b5
baharidvan <115007582+baharidvan@users.noreply.github.com>
2022-11-01 15:05:11 +0300

    Products CRUD Category CRUD User register-login Admin pages and operations Order Management

--- Abc.MvcWebUI/Entity/DataContext.cs
+++ Abc.MvcWebUI/Entity/DataContext.cs
@@ -10,10 +10,12 @@ namespace Abc.MvcWebUI.Entity
     {
         public DataContext() : base("dataConnection")
         {
-            Database.SetInitializer(new DataInitializer());
+            
         }
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderLine> OrderLines { get; set; }
     }
 }
---
+++ Abc.MvcWebUI/Identity/IdentityInitializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Abc.MvcWebUI.Entity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace Abc.MvcWebUI.Identity
+{
+    public class IdentityInitializer : CreateDatabaseIfNotExists<IdentityDataContext>
+    {
+        protected override void Seed(IdentityDataContext context)
+        {
+            // Rolleri
+            if (!context.Roles.Any(i => i.Name == "admin"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() {Name = "admin", Description = "admin rolü"};
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(i => i.Name == "user"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "user", Description = "user rolü" }; ;
+                manager.Create(role);
+            }
+            
+
+
+            base.Seed(context);
+        }
+    }
+}
